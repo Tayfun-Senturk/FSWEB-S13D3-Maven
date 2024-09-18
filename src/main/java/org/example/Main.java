7a737837f67b96bc33c65be8b3da8a352849b771
Tayfun-senturk <tayfunsenturk8@gmail.com>
2024-09-19 02:19:43 +0300

    first done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+//Soru-1
+        Person person = new Person("John", "Doe", 20);
+
+        System.out.println("Firstname: " + person.getFirstName());
+
+        System.out.println("LastName: " + person.getLastName());
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Person {
+    String firstname;
+    String lastName;
+    int age;
+    String nationality;
+    int Savings;
+
+    public Person(String firstname, String lastName, int age) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String firstname, String lastName, int age, String nationality, int Savings) {
+        this(firstname, lastName, age);
+        this.nationality = nationality;
+        this.Savings = Savings;
+    }
+
+    public String getFirstName() {
+        return this.firstname;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public boolean isTeen() {
+        return this.age < 19 && this.age > 13;
+    }
+}
